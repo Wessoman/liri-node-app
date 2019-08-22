07e53d99f887c79527dc32abf1548299a083f1a3
Antony Hernandez <wessoman@hotmail.com>
2019-08-22 08:50:51 -0500

    Added keys

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('LiriBot initiating.');
+
+exports.spotify = {
+  id: process.env.b07bad3837ef4d2991e9256fd60f39c7,
+  secret: process.env.d8492fb9186f463fae766bf7cd66f00f
+};
+
+exports.bandsintown = {
+  id: process.env.bff4f693be4e900b4369db7b509a27cc
+};
+
+exports.omdb = {
+  key: process.env.trilogy
+};
