2e06063518981ad0ade59b21d2e3f1cd650f58e5
Antony Hernandez <wessoman@hotmail.com>
2019-08-22 06:49:31 -0500

    added to liri.js

--- liri.js
+++ liri.js
@@ -14,3 +14,29 @@ let operator = process.argv[2];
 let queryArr = process.argv.slice(3);
 let logIt    = "[" + moment().format("MM/DD/YYYY h:mm:ss a") + "] " + process.argv.slice(2).join(" ");
 doSomething();
+
+function doSomething(){
+    if (operator == "do-what-it-says"){
+        let random = fs.readFileSync("./random.txt", "utf8");
+        random   = random.trim().split(",");
+        operator = random[0];
+        queryArr = [random[1]];
+        logIt   += " = " + operator + " " + queryArr;
+    }
+    if (operator == "concert-this"){
+        concertSearch();
+    } else if (operator == "spotify-this-song"){
+        getSpotifyData(client_id, client_secret);
+    } else if (operator == "movie-this"){
+        movieSearch();
+    } else {
+        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
+        console.log("To use liri type an operater in the command line");
+        console.log("(concert-this, spotify-this-song, movie-this, do-what-it-says)");
+        console.log("followed by your search term.");
+        console.log('example "node liri movie-this toy story"');
+        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
+    }
+    logIt += "\n";
+    fs.appendFile("./log.txt", logIt, err=>{if(err)console.log(err)});
+}
