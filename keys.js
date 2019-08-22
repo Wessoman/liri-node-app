d30551c33a33dd3cbd2a8fe2e81c96026622f25e
Antony Hernandez <wessoman@hotmail.com>
2019-08-22 11:13:17 -0500

    .env calls

--- keys.js
+++ keys.js
@@ -1,14 +1,14 @@
 console.log('LiriBot initiating.');
 
 exports.spotify = {
-  id: process.env.b07bad3837ef4d2991e9256fd60f39c7,
-  secret: process.env.d8492fb9186f463fae766bf7cd66f00f
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
 
 exports.bandsintown = {
-  id: process.env.bff4f693be4e900b4369db7b509a27cc
+  id: process.env.BANDSINTOWN_ID
 };
 
 exports.omdb = {
-  key: process.env.trilogy
+  key: process.env.OMDB_KEY
 };
--- liri.js
+++ liri.js
@@ -1,154 +1,85 @@
 require("dotenv").config();
-let fs     = require("fs");
-let axios  = require("axios");
-let keys   = require("./keys.js");
-let moment = require('moment');
-moment().format();
-let omdb            = keys.omdb;
-let bandsintown     = keys.bandsintown;
-let spotify         = keys.spotify;
-const client_id     = spotify.id;
-const client_secret = spotify.secret;
+var keys = require('./keys');
+var Spotify = require('node-spotify-api');
+//npm request - a way to make HTTP calls. A friend recommended.
+var request = require('request');
+var moment = require('moment');
+var spotify = new Spotify(keys.spotify)
 
-let operator = process.argv[2];
-let queryArr = process.argv.slice(3);
-let logIt    = "[" + moment().format("MM/DD/YYYY h:mm:ss a") + "] " + process.argv.slice(2).join(" ");
-doSomething();
 
-function doSomething(){
-    if (operator == "do-what-it-says"){
-        let random = fs.readFileSync("./random.txt", "utf8");
-        random   = random.trim().split(",");
-        operator = random[0];
-        queryArr = [random[1]];
-        logIt   += " = " + operator + " " + queryArr;
-    }
-    if (operator == "concert-this"){
-        concertSearch();
-    } else if (operator == "spotify-this-song"){
-        getSpotifyData(client_id, client_secret);
-    } else if (operator == "movie-this"){
-        movieSearch();
-    } else {
-        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        console.log("To use liri type an operater in the command line");
-        console.log("(concert-this, spotify-this-song, movie-this, do-what-it-says)");
-        console.log("followed by your search term.");
-        console.log('example "node liri movie-this toy story"');
-        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-    }
-    logIt += "\n";
-    fs.appendFile("./log.txt", logIt, err=>{if(err)console.log(err)});
-}
 
-function concertSearch(){
-    let query = "";
-    for (let c in queryArr){
-        query += queryArr[c].replace(/\s/g, "%20");
-        query += "%20";
-    }
-    query = query.replace(/%20\b/, "");
-    let queryURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=" + bandsintown.id;
+if (process.argv[2] == 'concert-this' ) {
+   
+    var performer = process.argv.slice(3).join(" ")
+    console.log(performer);
+   
+    var queryURL = "https://rest.bandsintown.com/artists/" + performer + "/events?app_id="+keys.bandsintown.id;
+
+    request(queryURL, function (error, response, body) {
+        if (error) console.log(error);
+
+        //JSON - exchange to/from server. JSON.parse (a method) similar to JSON.stringify
+        var result  =  JSON.parse(body)[0];
+        console.log("Venue name: " + result.venue.name);
+        console.log("Venue location: " + result.venue.city);
+        console.log("Date of Event: " +  moment(result.datetime).format("MM/DD/YYYY"));
+
+    });
     
-    axios.get(queryURL).then(function(response){
-        // console.log(response.data);
-        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        if (response.data[0] == undefined){
-            console.log("INVALID ARTIST OR NO UPCOMING SHOWS");
-            console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        } else {
-            for (let d=0; d<3; d++){
-                let date = moment(response.data[d].datetime).format("LL");
-                console.log(`Venue | ${response.data[d].venue.name}`);
-                console.log(`City  | ${response.data[d].venue.city}, ${response.data[d].venue.region}, ${response.data[d].venue.country}`);
-                console.log(`Date  | ${date}`);
-                console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
+} else if ( process.argv[2] == 'spotify-this-song') {
+//Nevermind [0] & [1] NOTE TO SELF - slice starting at 3
+    var songName = process.argv.slice(3).join(" ");
+
+    if (songName == undefined) {
+        songName = "The sign by Ace of Base";
+    } 
+   
+
+     spotify.search({ type: 'track', query: songName, limit: 10  }, function(err, data) {
+            if (err) {
+            return console.log('Error occurred: ' + err);
             }
-        }
+
+            var songArray = [];
+
+            for (var i = 0; i < data.tracks.items.length; i++ ) {
+                var result = {
+                    performer : data.tracks.items[i].album.artists[0].name,
+                    album_name : data.tracks.items[i].album.name,
+                    song_name : data.tracks.items[i].name,
+                    preview_url : data.tracks.items[i].preview_url 
+                }
+                songArray.push(result);
+            }
+            console.log(songArray);
+
+       
     });
+
+
+
+} else if ( process.argv[2] == 'movie-this') {
+    var movieName = process.argv.slice(3).join(" ");
+
+    if (movieName == undefined) {
+        movieName = "Mr. Nobody";
+    } 
+
+    request('http://www.omdbapi.com/?i=tt3896198&apikey=84741459&t=' + process.argv[3], function (error, response, body) {
+
+      var result  =  JSON.parse(body);
+      console.log("Title :" + result.Title);
+      console.log("Year :" + result.Released);
+      console.log("IMDB Rating :" + result.imdbRating );
+      console.log("Rotten Tomatoes :" + result.Ratings[0].Value);
+      console.log("Country :" +  result.Country);
+      console.log("Language :" + result.Language);
+      console.log("Movie Plot :" + result.Plot);
+      console.log("Actors :" +  result.Actors);
+
+  });
+
+} else if ( process.argv[2] == 'do-what-it-says') {
+  console.log('do what it says')
 }
 
-function getToken(client_id, client_secret){
-    return axios({
-        url: 'https://accounts.spotify.com/api/token',
-        method: 'post',
-        params: {
-            grant_type: 'client_credentials'
-        },
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        auth: {
-            username: client_id,
-            password: client_secret
-        }
-   });
-}
-
-async function getSpotifyData(client_id, client_secret){
-    const tokenData = await getToken(client_id, client_secret);
-    const token = tokenData.data.access_token;
-    
-    let query = "";
-    for (let s in queryArr){
-        query += queryArr[s].replace(/\s/g, "%20");
-        query += "%20";
-    }
-    query = query.replace(/%20\b/, "");
-    let queryURL = "https://api.spotify.com/v1/search?q=" + query + "&type=track&limit=3";
-
-    axios({
-        url: queryURL,
-        method: 'get',
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    }).then(response => {
-        // console.log(JSON.stringify(response.data, null, 2));
-        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        for (let t=0; t<3; t++){
-            console.log(`Artist    | ${response.data.tracks.items[t].artists[0].name}`);
-            console.log(`Song Name | ${response.data.tracks.items[t].name}`);
-            console.log(`Album     | ${response.data.tracks.items[t].album.name}`);
-            console.log(`Preview   | ${response.data.tracks.items[t].preview_url}`);
-            console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        }
-        return response.data;
-    }).catch(error => {
-        console.log("INVALID SONG TITLE");
-        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        return error;
-    });
-}
-
-function movieSearch(){
-    let query = "";
-    for (let c in queryArr){
-        query += queryArr[c].replace(/\s/g, "+");
-        query += "+";
-    }
-    query = query.replace(/\+$/, "");
-    if (query == ""){
-        query = "Mr.+Nobody";
-    }
-    let queryURL = "https://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=" + omdb.key;
-    
-    axios.get(queryURL).then(function(response){
-        // console.log(response.data);
-        console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        if (response.data.Response == 'False'){
-            console.log("INVALID MOVIE TITLE");
-            console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        } else {
-                console.log(`Title                  | ${response.data.Title}`);
-                console.log(`Year                   | ${response.data.Year}`);
-                console.log(`IMDB Rating            | ${response.data.Ratings[0].Value}`);
-                console.log(`Rotten Tomatoes Rating | ${response.data.Ratings[1].Value}`);
-                console.log(`Country                | ${response.data.Country}`);
-                console.log(`Language               | ${response.data.Language}`);
-                console.log(`Plot                   | ${response.data.Plot}`);
-                console.log(`Actors                 | ${response.data.Actors}`);
-                console.log(`XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`);
-        }
-    });
-}
